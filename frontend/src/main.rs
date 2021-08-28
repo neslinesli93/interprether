7fba73c34f39fd7d407cb586b665f8685f241600
Tommaso Pifferi <tommaso.pifferi@prima.it>
2021-08-28 22:03:48 +0200

    Add debounced search

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,6 +2,7 @@ use serde::Deserialize;
 use yew::format::{Json, Nothing};
 use yew::prelude::*;
 use yew::services::fetch::{FetchService, FetchTask, Request, Response};
+use yew::services::timeout::{TimeoutService, TimeoutTask};
 use yew::virtual_dom::{VList, VNode};
 
 const BACKEND_URL: &str = "http://localhost:3030";
@@ -29,10 +30,12 @@ impl Transaction {
                         </a>
                     </button>
                 </header>
-                <div class="card-content">
-                    <div class="content">
-                        { &self.message }
-                    </div>
+                <div>
+                    <figure class="highlight">
+                        <pre>
+                            <code>{ &self.message }</code>
+                        </pre>
+                    </figure>
                 </div>
             </div>
         }
@@ -43,14 +46,18 @@ enum Msg {
     FetchTransactions,
     TransactionsFetched(Vec<Transaction>),
     HttpError(String),
+    DebounceFilter(String),
+    EditFilter(String),
 }
 
 struct Model {
     transactions: Vec<Transaction>,
     loading: bool,
     error: Option<String>,
+    filter: Option<String>,
     link: ComponentLink<Self>,
     fetch_task: Option<FetchTask>,
+    timeout_task: Option<TimeoutTask>,
 }
 
 impl Model {
@@ -75,7 +82,7 @@ impl Model {
 
     fn view_loading(&self) -> Html {
         if self.loading {
-            html! { "Loading..."}
+            html! { <div>{ "Loading..." }</div> }
         } else {
             VNode::from(VList::new())
         }
@@ -94,8 +101,10 @@ impl Component for Model {
             transactions: vec![],
             loading: false,
             error: None,
+            filter: None,
             link,
             fetch_task: None,
+            timeout_task: None,
         }
     }
 
@@ -120,6 +129,24 @@ impl Component for Model {
 
                 true
             }
+            Msg::DebounceFilter(filter) => {
+                self.timeout_task = None;
+
+                let cb = self.link.callback(move |_| Msg::EditFilter(filter.to_owned()));
+                let timeout_task = TimeoutService::spawn(std::time::Duration::from_millis(300), cb);
+                self.timeout_task = Some(timeout_task);
+
+                false
+            }
+            Msg::EditFilter(filter) => {
+                if filter.trim().is_empty() {
+                    self.filter = None;
+                } else {
+                    self.filter = Some(filter.trim().into())
+                }
+
+                true
+            }
         }
     }
 
@@ -128,12 +155,25 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
-        html! {
-            <section class="section">
-                {self.view_loading()}
+        let oninput = self.link.callback(|event: InputData| Msg::DebounceFilter(event.value));
 
-                {for self.transactions.iter().map(|tx| tx.render())}
-            </section>
+        let filter = |tx: &&Transaction| {
+            self.filter
+                .as_ref()
+                .map(|f| tx.message.to_lowercase().contains(&f.to_lowercase()))
+                .unwrap_or(true)
+        };
+
+        html! {
+            <div class="container">
+                <section class="section">
+                    {self.view_loading()}
+
+                    <input class="input" type="search" placeholder="Search transactions" oninput=oninput />
+
+                    {for self.transactions.iter().filter(filter).map(|tx| tx.render())}
+                </section>
+            </div>
         }
     }
 }
