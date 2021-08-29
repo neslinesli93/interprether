e08879e94954b183b460a4f35146a303cfe3798c
Tommaso Pifferi <tommaso.pifferi@prima.it>
2021-08-29 10:49:15 +0200

    Improve Transaction struct, and add timestamp

--- src/block.rs
+++ src/block.rs
@@ -1,17 +1,22 @@
 use serde::{Deserialize, Serialize};
 use web3::types::H256;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Transaction {
+    #[serde(rename = "h")]
     pub hash: String,
+    #[serde(rename = "m")]
     pub message: String,
+    #[serde(rename = "t")]
+    pub timestamp: u64,
 }
 
 impl Transaction {
-    pub fn new(hash: H256, message: &str) -> Self {
+    pub fn new(hash: H256, message: String, timestamp: u64) -> Self {
         Transaction {
             hash: format!("{:?}", hash),
-            message: message.trim().into(),
+            message,
+            timestamp,
         }
     }
 }
