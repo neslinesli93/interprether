30a26f8e456ea44b29040d50442d9a5e130708e5
Tommaso Pifferi <tommaso.pifferi@prima.it>
2021-09-01 21:04:32 +0200

    WIP: animate new txs and some new bins (seed, fake_tx)

---
+++ fake_tx.sh
@@ -0,0 +1,7 @@
+#!/bin/env sh
+
+while true
+do
+  ./target/release/fake_tx
+  sleep 30
+done
---
+++ src/bin/fake_tx.rs
@@ -0,0 +1,38 @@
+use anyhow::Result;
+use eth_oracle_rs::{block, redis};
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn rand_string(length: usize) -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(length)
+        .map(char::from)
+        .collect()
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let mut rng = rand::thread_rng();
+
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+
+    let transactions = vec![block::Transaction {
+        hash: format!("0x{}", rand_string(32)),
+        message: rand_string(rng.gen_range(50..100)),
+        timestamp: now.as_secs(),
+    }];
+
+    let serialized_tx = serde_json::to_string(&transactions)?;
+    redis::zadd(now.as_secs().into(), serialized_tx).await?;
+
+    log::info!("Inserted tx at {}", now.as_secs());
+
+    Ok(())
+}
