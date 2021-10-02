05983288d7950389f5b1351ac896cac921935c8e
Tommaso Pifferi <tommaso.pifferi@prima.it>
2021-10-02 11:33:38 +0200

    Add highlight for matches

---
+++ frontend/src/string.rs
@@ -0,0 +1,49 @@
+use yew::prelude::*;
+
+pub struct StringPart {
+    pub s: String,
+    pub t: StringPartType,
+}
+
+#[derive(PartialEq)]
+pub enum StringPartType {
+    Normal,
+    Highlight,
+}
+
+pub fn split_keep<'a>(text: &'a str, r: &'a str) -> Vec<StringPart> {
+    let mut result = Vec::new();
+    let mut last = 0;
+    for (index, matched) in text.to_lowercase().match_indices(&r) {
+        if last != index {
+            result.push(StringPart {
+                s: text[last..index].to_string(),
+                t: StringPartType::Normal,
+            });
+        }
+
+        last = index + matched.len();
+        result.push(StringPart {
+            s: text[index..last].to_string(),
+            t: StringPartType::Highlight,
+        });
+    }
+
+    if last < text.len() {
+        result.push(StringPart {
+            s: text[last..].to_string(),
+            t: StringPartType::Normal,
+        });
+    }
+
+    result
+}
+
+impl StringPart {
+    pub fn render(&self) -> Html {
+        match self.t {
+            StringPartType::Normal => html! { <span> { &self.s } </span> },
+            StringPartType::Highlight => html! { <mark> { &self.s } </mark> },
+        }
+    }
+}
